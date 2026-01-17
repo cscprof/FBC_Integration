74485ae226495ab21d1fd81d1ad18e9a475bc1e1
Owen Bacon <owen.bacon2006@gmail.com>
2026-01-17 17:24:29 -0500

    Added a conf example and fixed typos to make resource directory reachable

--- app/resources/views.py
+++ app/resources/views.py
@@ -3,6 +3,6 @@ from flask import render_template
 from . import resources
 
 # Use the route() decorator to tell Flask what URL should trigger the function
-@resources.route("/resources")
+@resources.route("/resource-directory")
 def home_page():
     return render_template("resources/resourceDirectory.html")
