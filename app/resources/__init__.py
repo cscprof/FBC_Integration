82852d778d125add856831748cebdf98c7bf5bb7
owenb <owen.bacon2006@gmail.com>
2026-01-20 11:47:23 -0500

    Renamed branch

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -3,5 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 resources = Blueprint('resources', __name__, template_folder='templates')
-
+#Have to make db here somehow
 from . import views
