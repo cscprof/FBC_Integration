f1164ff8c9c5f0878cd9b60833d2b0b1f85de254
InTheOvven <owen.bacon2006@gmail.com>
2026-02-26 10:26:23 -0500

    Backend functional

--- app/resources/views.py
+++ app/resources/views.py
@@ -26,6 +26,9 @@ def resourcesearch():
             resources.url,
             resources.resource_category_id,
             resource_category.resource_category_name,
+            resources.contact_name,
+            resources.contact_email,
+            resources.contact_phone,
         )
         .join(
             resource_category,
@@ -40,7 +43,10 @@ def resourcesearch():
         {
             'description': 'Geneva College Financial Aid Website',
             'url': 'https://www.geneva.edu/financial-aid/',
-            'resource_category_name': 'college'
+            'resource_category_name': 'college',
+            'contact_name' : 'Dean Swank',
+            'contact_email' : 'dswank@geneva.edu',
+            'contact_phone' : '18005882300'
         },
         {
             'description': 'Geneva Application Process',
