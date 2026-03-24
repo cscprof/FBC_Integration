4ec9a61fa458e3ed8ffd950f8dba1c2f5eecfb26
Noah Hammond <156326445+TheDoctor50208@users.noreply.github.com>
2026-03-24 10:04:28 -0400

    Delete FBC_Integration-main directory

--- FBC_Integration-main/app/users/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-users = Blueprint('users', __name__, template_folder='templates')
-
-# signup = Blueprint('signup', __name__, template_folder='templates')
-# login = Blueprint('login', __name__, template_folder='templates')
-# adminpanel = Blueprint('adminpanel', __name__, template_folder='templates')
-
-from . import views, Hashing  # Attaches routes
