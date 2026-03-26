ba216c19a88c452850149542f84d27fcf5335c19
Katelynn Bell <knbell@geneva.edu>
2026-03-26 12:52:19 -0400

    admin edit user form ui changes

--- app/users/views.py
+++ app/users/views.py
@@ -166,7 +166,7 @@ def edit_user(user_id):
                       username, role_id, user_id))
                 conn.commit()
                 flash("User updated!", "success")
-                return redirect(url_for("users.admin_panel"))
+                return redirect(url_for("users.admin_users"))
 
             cursor.execute("SELECT * FROM users WHERE user_id=%s", (user_id,))
             user = cursor.fetchone()
@@ -176,5 +176,5 @@ def edit_user(user_id):
 
     if not user:
         flash("User not found")
-        return redirect(url_for("users.admin_panel"))
+        return redirect(url_for("users.admin_users"))
     return render_template("adminpanel/edit_user.html", user=user)
